3df2b9c61338679386503d45d3d0e9b21573fd49
lydh3 <LyDH3@fsoft.com.vn>
2018-06-02 23:37:58 +0700

    Update: Add card number/details mapping data

--- src/SingleCardView/SingleCardView.js
+++ src/SingleCardView/SingleCardView.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import cardMappingData from "../data/cardMapping";
 
 export default class SingleCardView extends React.Component {
 
-	getRandomCardNumber(){
-		return Math.floor(Math.random() * 78) + 1  ;
+	getRandomCardNumber() {
+		return Math.floor(Math.random() * 22) + 1;
 	}
+
+	getCardInfo(cardNumber) {
+		const cardIndex = cardNumber - 1;
+		return cardMappingData[cardIndex];
+	}
+
+	getRandomCardInfo() {
+		const randomNumber = this.getRandomCardNumber();
+		return this.getCardInfo(randomNumber);
+	}
+
 
 	render() {
 
-		const randomNumber = this.getRandomCardNumber();
+		const randomCardInfo = this.getRandomCardInfo();
+		const {title, upright, reversed, cardImage} = randomCardInfo;
+		const imagePath = `./img/${cardImage}`;
 
 		return (
 			<div className="container">
 				<div className="text-center">
-					<p className="lead mt-5">{randomNumber}</p>
+					<h3>{title}</h3>
+					<div className="mb-1">
+						<img className="img-fluid" src={imagePath} alt="Card Image"/>
+					</div>
+					<p className="text-center">
+						{upright}
+					</p>
 				</div>
 			</div>
 		)
